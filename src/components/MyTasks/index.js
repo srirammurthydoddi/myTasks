02a683a6a5909f311e198c78194d08d67fc983a3
srirammurthydoddi <sriram1996.sr07@gmail.com>
2023-02-21 11:07:43 +0000

    myTasks

--- src/components/MyTasks/index.js
+++ src/components/MyTasks/index.js
@@ -1,10 +1,30 @@
 import {Component} from 'react'
-
 import {v4} from 'uuid'
 
-import TaskItem from '../TaskItem'
+import {
+  HomeContainer,
+  CreateTaskContainer,
+  CreateForm,
+  FormHeading,
+  LabelInputContainer,
+  Label,
+  Input,
+  SelectInput,
+  OptionInput,
+  Button,
+  AddTaskContainer,
+  TagsHeading,
+  TagsListUl,
+  TagsList,
+  TagsButton,
+  TaskListUl,
+  TaskListLi,
+  TaskText,
+  TaskTag,
+  NoTaskText,
+} from './styledComponents'
 
-import './index.css'
+// These are the lists used in the application. You can move them to any component needed.
 
 const tagsList = [
   {
@@ -35,103 +55,142 @@ const tagsList = [
 
 class MyTasks extends Component {
   state = {
-    taskInput: '',
-    activeTagId: tagsList[0].optionId,
-    tasksList: [],
+    inputText: '',
+    inputTag: tagsList[0].optionId,
+    taskList: [],
+    activeTag: 'INITIAL',
   }
 
-  onAddTask = event => {
-    event.preventDefault()
-    const {taskInput, activeTagId} = this.state
+  changeInput = event => {
+    this.setState({inputText: event.target.value})
+  }
 
+  onChangeTag = event => {
+    this.setState({inputTag: event.target.value})
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+    const {inputText, inputTag} = this.state
     const newTask = {
       id: v4(),
-      task: taskInput,
-      tag: activeTagId,
+      task: inputText,
+      tag: inputTag,
     }
+    if (inputText.length !== 0) {
+      this.setState(prevState => ({
+        taskList: [...prevState.taskList, newTask],
+        inputText: '',
+        inputTag: '',
+      }))
+    }
+  }
 
+  onClickActiveTag = event => {
     this.setState(prevState => ({
-      tasksList: [...prevState.tasksList, newTask],
-      taskInput: '',
-      activeTagId: '',
+      activeTag:
+        prevState.activeTag === event.target.value
+          ? 'INITIAL'
+          : event.target.value,
     }))
   }
 
-  onChangeTaskInput = event => {
-    this.setState({
-      taskInput: event.target.value,
-    })
+  renderCreateTaskView = () => {
+    const {inputText, inputTag} = this.state
+    return (
+      <CreateTaskContainer>
+        <CreateForm onSubmit={this.submitForm}>
+          <FormHeading>Create a task!</FormHeading>
+          <LabelInputContainer>
+            <Label htmlFor="inputText">Task</Label>
+            <Input
+              type="text"
+              placeholder="Enter the task here"
+              onChange={this.changeInput}
+              value={inputText}
+              id="inputText"
+            />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label htmlFor="selectTag">Tags</Label>
+            <SelectInput
+              onChange={this.onChangeTag}
+              value={inputTag}
+              id="selectTag"
+            >
+              {tagsList.map(each => (
+                <OptionInput value={each.optionId} key={each.optionId}>
+                  {each.displayText}
+                </OptionInput>
+              ))}
+            </SelectInput>
+          </LabelInputContainer>
+          <Button type="submit">Add Task</Button>
+        </CreateForm>
+      </CreateTaskContainer>
+    )
   }
 
-  onChangeTagInput = event => {
-    this.setState({activeTagId: event.target.value})
+  renderTaskCard = () => {
+    const {taskList, activeTag} = this.state
+    const filterTaskList =
+      activeTag === 'INITIAL'
+        ? taskList
+        : taskList.filter(each => each.tag === activeTag)
+    return (
+      <>
+        {filterTaskList.map(each => (
+          <TaskListLi key={each.id}>
+            <TaskText>{each.task}</TaskText>
+            <TaskTag>{each.tag}</TaskTag>
+          </TaskListLi>
+        ))}
+      </>
+    )
+  }
+
+  renderAddTaskView = () => {
+    const {taskList, activeTag} = this.state
+
+    return (
+      <AddTaskContainer>
+        <TagsHeading>Tags</TagsHeading>
+        <TagsListUl>
+          {tagsList.map(each => {
+            const isActive = activeTag === each.optionId
+            return (
+              <TagsList key={each.optionId}>
+                <TagsButton
+                  type="button"
+                  value={each.optionId}
+                  onClick={this.onClickActiveTag}
+                  isActive={isActive}
+                >
+                  {each.displayText}
+                </TagsButton>
+              </TagsList>
+            )
+          })}
+        </TagsListUl>
+        <TagsHeading>Tasks</TagsHeading>
+        <TaskListUl>
+          {taskList.length === 0 ? (
+            <NoTaskText>No Tasks Added Yet</NoTaskText>
+          ) : (
+            this.renderTaskCard()
+          )}
+        </TaskListUl>
+      </AddTaskContainer>
+    )
   }
 
   render() {
-    const {taskInput, activeTagId, tasksList} = this.state
     return (
-      <div className="app-container">
-        <div className="task-container">
-          <form className="form" onSubmit={this.onAddTask}>
-            <h1 className="heading">Create a task!</h1>
-            <label htmlFor="task" className="label">
-              Task
-            </label>
-            <input
-              type="text"
-              id="task"
-              className="input-text"
-              placeholder="Enter the task here"
-              value={taskInput}
-              onChange={this.onChangeTaskInput}
-            />
-            <label htmlFor="task" className="label">
-              Tags
-            </label>
-            <select
-              className="tag-select"
-              onChange={this.onChangeTagInput}
-              value={activeTagId}
-            >
-              {tagsList.map(eachTag => (
-                <option
-                  key={eachTag.optionId}
-                  value={eachTag.optionId}
-                  className="option"
-                >
-                  {eachTag.displayText}
-                </option>
-              ))}
-            </select>
-            <button type="submit" className="add-button">
-              Add Task
-            </button>
-          </form>
-        </div>
-        <div className="tags-container">
-          <h1 className="tags-heading">Tags</h1>
-          <ul className="tags-list">
-            {tagsList.map(eachTag => (
-              <button
-                type="button"
-                className="button"
-                key={eachTag.id}
-                value={eachTag.id}
-              >
-                {eachTag.displayText}
-              </button>
-            ))}
-          </ul>
-          <h1 className="tasks-heading">Tasks</h1>
-          <ul className="tasks-list">
-            {tasksList.map(eachTask => (
-              <TaskItem key={eachTask.id} TaskDetails={eachTask} />
-            ))}
-          </ul>
-        </div>
-      </div>
+      <HomeContainer>
+        {this.renderCreateTaskView()}
+        {this.renderAddTaskView()}
+      </HomeContainer>
     )
   }
 }
-
 export default MyTasks
